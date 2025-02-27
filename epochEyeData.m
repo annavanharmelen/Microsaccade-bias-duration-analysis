ec569d3ba79fc75ae3bc12e004e535d412c175bc
Anna <anna@vanharmelen.me>
2025-02-27 15:18:56 +0100

    remove obsolete code

--- epochEyeData.m
+++ epochEyeData.m
@@ -3,12 +3,8 @@
 %% start clean
 clear; clc; close all;
 
-%% settings
-nan_trial_overlap = 0;
-nan_post_target = 1;
-
 %% set loops
-for pp      = [1:2];
+for pp      = [1];
 
     %% Set trig labels and epoch timings
     values2use  = 31:34; % cue onset
@@ -75,52 +71,6 @@ for pp      = [1:2];
     end
     eyedata.trialinfo(:,1) = trigval';
 
-    %% NaN all data after response trigger
-    if nan_post_target
-        trial_end_samples = [];
-        trial_end_trigger = {};
-
-        % Get final triggers + corresponding samples from each trial
-        % NOTE: THIS DOESN'T REMOVE TRIALS THAT END IN A 10.. TRIGGER
-        % (indicating trial interruption by fixational control)
-        for trig_idx = 1:length(event.label(trloi))
-            trial_end_samples(trig_idx) = event.sample(trloi(trig_idx) + 1);
-            trial_end_trigger(trig_idx) = event.label(trloi(trig_idx) + 1);
-        end
-
-        % NaN all data from trigger sample onwards
-        for trial_idx = 1:length(eyedata.sampleinfo)
-            if trial_end_samples(trial_idx) < eyedata.sampleinfo(trial_idx,2)
-                time_diff = eyedata.sampleinfo(trial_idx,2) - trial_end_samples(trial_idx);
-                eyedata.trial{trial_idx}(:,end-(time_diff-1):end) = nan(size(eyedata.trial{trial_idx}(:,end-(time_diff-1):end)));
-            end
-        end
-    end
-
-    %% NaN all data after orientation change
-    if nan_trial_overlap
-        trial_end_samples = [];
-        trial_end_trigger = {};
-
-        % Get final triggers + corresponding samples from each trial
-        for trig_idx = 1:length(event.label(trloi))-1
-            trial_end_samples(trig_idx) = event.sample(trloi(trig_idx + 1) - 2);
-            trial_end_trigger(trig_idx) = event.label(trloi(trig_idx + 1) - 2);
-        end
-
-        % Get final trigger + sample
-        trial_end_samples(end+1) = event.sample(end);
-        trial_end_trigger(end+1) = event.label(end);
-
-        % NaN all data from trigger sample onwards
-        for trial_idx = 1:length(eyedata.sampleinfo)
-            if trial_end_samples(trial_idx) < eyedata.sampleinfo(trial_idx,2)
-                time_diff = eyedata.sampleinfo(trial_idx,2) - trial_end_samples(trial_idx);
-                eyedata.trial{trial_idx}(:,end-(time_diff-1):end) = nan(size(eyedata.trial{trial_idx}(:,end-(time_diff-1):end)));
-            end
-        end
-    end
-
     %% get to three channels
     % we only need x-axis, y-axis, & pupil from now on
     eyedata.label(2:4) = {'eyeX','eyeY','eyePupil'};
@@ -131,27 +81,14 @@ for pp      = [1:2];
     eyedata = ft_selectdata(cfg, eyedata);
     
     %% save data as function of pp name and eyedata session
-    % depending on this option, append to name of saved file. 
-    if nan_trial_overlap == 1
-        toadd1 = '_NaNtrialoverlap';
-    else
-        toadd1 = '';
-    end
-    
-    if nan_post_target == 1
-        toadd2 = '_NaNposttarget';
-    else
-        toadd2 = '';
-    end    
-
-    save([param.path, '\epoched_data\eyedata_AnnaMicro2', toadd1, toadd2, '__', param.subjName], 'eyedata');
+    save([param.path, '\epoched_data\eyedata_m4', '__', param.subjName], 'eyedata');
     
     %% test plot
-    % figure; 
-    % subplot(2,2,1); plot(eyedata.time{1}, eyedata.trial{1}); legend(eyedata.label);
-    % subplot(2,2,2); plot(eyedata.time{10}, eyedata.trial{10}); legend(eyedata.label);
-    % subplot(2,2,3); plot(eyedata.time{100}, eyedata.trial{100}); legend(eyedata.label);
-    % subplot(2,2,4); plot(eyedata.time{200}, eyedata.trial{200}); legend(eyedata.label); 
-    % 
+    figure; 
+    subplot(2,2,1); plot(eyedata.time{1}, eyedata.trial{1}); legend(eyedata.label);
+    subplot(2,2,2); plot(eyedata.time{10}, eyedata.trial{10}); legend(eyedata.label);
+    subplot(2,2,3); plot(eyedata.time{100}, eyedata.trial{100}); legend(eyedata.label);
+    subplot(2,2,4); plot(eyedata.time{200}, eyedata.trial{200}); legend(eyedata.label); 
+
 %% end loops
 end % end of pp loop
