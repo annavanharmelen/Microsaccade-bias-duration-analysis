f4c28d1810f2a16b0f65d2d9e1998e69dadc0622
Anna <anna@vanharmelen.me>
2025-05-12 14:19:59 +0200

    add unique ids

--- getSubjParam.m
+++ getSubjParam.m
@@ -3,7 +3,7 @@ function param = getSubjParam(pp)
 %% participant-specific notes
 
 %% set path and pp-specific file locations
-unique_numbers = [25, 10, 33, 43, 92]; %needs to be in the right order
+unique_numbers = [25, 10, 33, 43, 92, 91, 59, 44, 32, 41, 17, 38, 70, 46, 47, 76, 40, 57, 45, 27, 80, 22]; %needs to be in the right order
 
 param.path = '\\labsdfs.labs.vu.nl\labsdfs\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Anna\Data\m4 - duration v2\';
 
